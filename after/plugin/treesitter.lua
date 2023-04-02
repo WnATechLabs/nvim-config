fe1dd3232ae586306a16494ab201097589ad5137
Will <william@wnatechlabs.com>
2023-04-02 18:37:54 -0400

    add treesitter code folding

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -20,3 +20,8 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+-- Code folding
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"

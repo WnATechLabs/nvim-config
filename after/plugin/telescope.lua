1a6e6b6ca8b5bdc6ba6170765de4508375872e25
Will <william@wnatechlabs.com>
2023-04-03 12:07:15 -0400

    update telescope remaps to show dotfiles

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+local opts = {
+    hidden=true,
+}
+
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files(opts) end, {})
+vim.keymap.set('n', '<C-p>', function() builtin.git_files(opts) end, {})
+vim.keymap.set('n', '<leader>ps', function() builtin.live_grep(opts) end, {})

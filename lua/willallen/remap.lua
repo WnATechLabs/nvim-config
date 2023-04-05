d77df35d7ba166021e20c12f940de6b9f925d6a5
Will <william@wnatechlabs.com>
2023-04-05 12:40:32 -0400

    add remaps for tab and split management

--- lua/willallen/remap.lua
+++ lua/willallen/remap.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- next greatest remap ever
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
@@ -28,3 +28,24 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Tab management
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>")
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>")
+vim.keymap.set("n", "<leader>to", "<cmd>tabonly<CR>")
+
+-- Split management
+vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>")
+vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>")
+vim.keymap.set("n", "<leader>sc", "<cmd>close<CR>")
+vim.keymap.set("n", "<leader>so", "<cmd>only<CR>")
+-- -- Use ctrl + up/down/left/right to resize windows by 3
+vim.keymap.set("n", "<C-Up>", "<cmd>resize -3<CR>")
+vim.keymap.set("n", "<C-Down>", "<cmd>resize +3<CR>")
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -3<CR>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +3<CR>")
+-- -- Use ctrl + hjkl to move between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")

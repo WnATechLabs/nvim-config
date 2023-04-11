a0363f67091a4dc8424c7d8da910c7559c8297db
Will <william@wnatechlabs.com>
2023-04-11 11:36:35 -0400

    add trouble plugin

--- lua/willallen/packer.lua
+++ lua/willallen/packer.lua
@@ -76,6 +76,11 @@ return require('packer').startup(function(use)
         requires = { "nvim-lua/plenary.nvim" },
     }
 
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+    }
+
     -- end general plugins
 
     -- LSP

2b9451106b7bde41efada1c388179efc44cdd988
Will <william@wnatechlabs.com>
2023-04-12 12:55:24 -0400

    set NormalNC background to none

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,9 +2,9 @@ function ColorMe(color)
 	color = color or "nightfox"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 ColorMe()

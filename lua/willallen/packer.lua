cc720e2c3e419c696beaccb3149fa11b5d3f8f6e
Will <william@wnatechlabs.com>
2023-04-22 12:16:36 -0400

    add galaxyline plugin

--- lua/willallen/packer.lua
+++ lua/willallen/packer.lua
@@ -1,126 +1,143 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
+-- vim.cmd [[packadd packer.nvim]]
 require('packer').init {
-    git = { clone_timeout = 60 * 60 * 60 }
+  git = { clone_timeout = 60 * 60 * 60 }
 }
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use('wbthomason/packer.nvim')
+  -- Packer can manage itself
+  use('wbthomason/packer.nvim')
 
-    -- Themes
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine'
-    })
+  -- Themes
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine'
+  })
 
-    use({
-        "EdenEast/nightfox.nvim",
-        config = function()
-            vim.cmd("colorscheme nightfox")
-        end
-    })
-    -- end themes
+  use({"EdenEast/nightfox.nvim"})
+  -- end themes
 
-    -- General plugins to make my life easier
-    use('github/copilot.vim')
+  -- General plugins to make my life easier
+  use('github/copilot.vim')
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+  use('nvim-lua/plenary.nvim')
+
+  use('ThePrimeagen/harpoon')
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use({
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup {}
+    end,
+  })
+
+  use({
+    "kylechui/nvim-surround",
+    tag = "*",     -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
+
+  use({
+    "terrortylor/nvim-comment",
+    config = function()
+      require('nvim_comment').setup()
+    end
+  })
+
+  use('gerazov/toggle-bool.nvim')
+
+  -- -- HTTP Client
+  use {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+  }
+
+  use {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+  }
+
+  use('christoomey/vim-tmux-navigator')
+
+  use('mrjones2014/nvim-ts-rainbow')
+
+  use {
+    'ZhiyuanLck/smart-pairs',
+    event = 'InsertEnter',
+    config = function()
+      require('pairs'):setup()
+    end
+  }
+
+  use ('RRethy/vim-illuminate')
+
+  use({
+    "NTBBloodbath/galaxyline.nvim",
+    -- your statusline
+    config = function()
+      require("galaxyline.themes.eviline")
+    end,
+    -- some optional icons
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
+  })
+
+  -- end general plugins
+
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      -- Snippet Collection (Optional)
+      { 'rafamadriz/friendly-snippets' },
     }
+  }
+  -- end LSP
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-    use('nvim-lua/plenary.nvim')
-
-    use('ThePrimeagen/harpoon')
-
-    use('mbbill/undotree')
-
-    use('tpope/vim-fugitive')
-
-    use({
-        "Pocco81/auto-save.nvim",
-        config = function()
-            require("auto-save").setup {}
-        end,
-    })
-
-    use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    })
-
-    use({
-        "terrortylor/nvim-comment",
-        config = function()
-            require('nvim_comment').setup()
-        end
-    })
-
-    use ('gerazov/toggle-bool.nvim')
-
-    -- -- HTTP Client
-    use {
-        "rest-nvim/rest.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-    }
-
-    use {
-        "folke/trouble.nvim",
-        requires = "nvim-tree/nvim-web-devicons",
-    }
-
-    -- end general plugins
-
-    -- LSP
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            -- Snippet Collection (Optional)
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
-    -- end LSP
-
-    -- DAP
-    use {
-        "mfussenegger/nvim-dap",
-        opt = true,
-        module = { "dap" },
-        requires = {
-            "theHamsta/nvim-dap-virtual-text",
-            "rcarriga/nvim-dap-ui",
-            "nvim-telescope/telescope-dap.nvim",
-            -- { "jbyuki/one-small-step-for-vimkind", module = "osv" },
-        },
-        disable = false,
-    }
-    -- end DAP
+  -- DAP
+  use {
+    "mfussenegger/nvim-dap",
+    opt = true,
+    module = { "dap" },
+    requires = {
+      "theHamsta/nvim-dap-virtual-text",
+      "rcarriga/nvim-dap-ui",
+      "nvim-telescope/telescope-dap.nvim",
+      -- { "jbyuki/one-small-step-for-vimkind", module = "osv" },
+    },
+    disable = false,
+  }
+  -- end DAP
 end)
-

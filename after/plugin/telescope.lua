1ef51469168361346e55c5de9267307a9f4736d4
Will <william@wnatechlabs.com>
2023-05-06 13:29:37 -0400

    update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 local opts = {
     hidden=true,
+    -- no_ignore=true,
 }
 
 vim.keymap.set('n', '<leader>pf', function() builtin.find_files(opts) end, {})
